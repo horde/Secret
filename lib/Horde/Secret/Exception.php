9d9d9d45c7639d6fe055df3215ad961b921d2a6f
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-03 11:15:10 +0100

    chore: update PSR-0 compatibility layer and add integration tests
    Update copyright years in PSR-0 implementation and add comprehensive
    integration tests verifying PSR-0 and PSR-4 can coexist.
    
    Changes:
    - Update copyright to 2026 in lib/Horde/Secret.php and Exception.php
    - Add documentation to PSR-0 Exception about BC maintenance
    - Add 7 integration tests verifying interoperability
    
    PSR-0 API remains completely unchanged - drop-in compatible upgrade.
    
    Test results: 111 tests, 213 assertions, all pass

--- lib/Horde/Secret/Exception.php
+++ lib/Horde/Secret/Exception.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * Exception handler for the Horde_Secret library.
  *
- * Copyright 2010-2017 Horde LLC (http://www.horde.org/)
+ * Copyright 2010-2026 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
@@ -12,9 +13,20 @@
  * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @package  Secret
  */
+
+/**
+ * PSR-0 Exception class for Horde_Secret.
+ *
+ * Maintains backward compatibility with existing code while
+ * optionally integrating with PSR-4 exception hierarchy.
+ *
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Secret
+ */
 class Horde_Secret_Exception extends Horde_Exception_Wrapped
 {
-    // Error codes.
+    // Error codes for backward compatibility
     const NO_BLOWFISH_LIB = 0; // 0 for BC
     const KEY_NOT_STRING = 2;
     const KEY_ZERO_LENGTH = 3;
